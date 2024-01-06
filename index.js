4d9e0832a29b629784ec3e08cb7426351bc0846f
MorganNjoroge <zeejaymorgan@morgan.com>
2024-01-06 23:45:14 +0300

    dom elements

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML='<h1>YOUR-NAME is the champion</h1>'
+//document.body.appendChild(newHeader);
+//console.log (newHeader);
+
+
